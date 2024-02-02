dccc52632dfc65e8a6efd481a92826cbc63a2844
Williamiamiam <william.rech@epitech.eu>
2024-02-02 16:15:14 +0100

    fix camera

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -7,17 +7,13 @@ public class NetworkCamera : NetworkBehaviour
 {
     Camera baseplateCam;
 
-    public override void OnStartClient()
+    public override void OnStartLocalPlayer()
     {
-        base.OnStartClient();
+        base.OnStartLocalPlayer();
         baseplateCam = GetComponentInChildren<Camera>();
         if (isLocalPlayer)
         {
             baseplateCam.rect = new Rect(0.068f, 0.025f, 0.24f, 0.33f);
-        }
-        else
-        {
-            baseplateCam.rect = new Rect(0.655f, 0.025f, 0.24f, 0.33f);
         }
     }
 }
