2b3f2d0e517dcf9499d4cb0491117fb1d184c2d6
Williamiamiam <william.rech@epitech.eu>
2024-02-02 17:53:30 +0100

    player name tests

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -18,10 +18,8 @@ namespace QuickStart
         {
             if (!isLocalPlayer)
                 return;
-
             float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * 110.0f;
             float moveZ = Input.GetAxis("Vertical") * Time.deltaTime * 4f;
-
             transform.Rotate(0, moveX, 0);
             transform.Translate(0, 0, moveZ);
         }
